09a84b48b5664aa27af98e773403c415c82368cd
iTrooz <hey@itrooz.fr>
2025-11-11 02:30:13 +0100

    add clang CI + add Linux CI + remove `__cpp_modules`

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -10,16 +10,5 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_COMPILER_VERSION VER
 # https://releases.llvm.org/17.0.1/tools/clang/docs/StandardCPlusPlusModules.html#how-to-enable-standard-c-modules
 endif()
 
-check_cxx_symbol_exists(__cpp_modules "" HAVE_CXX_MODULES)
-# Clang 19 didn't have __cpp_modules
-
-# GCC 15+, ...
-message(STATUS "C++ modules CMAKE_CXX_COMPILER_IMPORT_STD: ${CMAKE_CXX_COMPILER_IMPORT_STD}")
-check_cxx_symbol_exists(__cpp_lib_modules "version" HAVE_STD_MODULES)
-
-if(NOT HAVE_STD_MODULES AND NOT HAVE_CXX_MODULES)
-  message(FATAL_ERROR "${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION} does not support C++20 modules")
-endif()
-
 check_source_compiles(CXX "import std;
 int main() { return 0; }" HAVE_CXX_IMPORT_STD)
